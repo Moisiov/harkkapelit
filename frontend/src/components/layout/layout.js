ac4876550e5a5eb6a1dd43fe29a40ba78d7419de
Valto Moisio <valto.moisio@pilvit.fi>
2022-04-25 13:08:50 +0300

    Add basic layout component

---
+++ frontend/src/components/layout/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = ({ children }) => {
+    return(
+        <>
+            <div>
+                Layout component
+            </div>
+        </>
+    )
+}
+
+export default Layout;
