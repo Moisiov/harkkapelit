136fb300a70a11fe92d5371e31b68ad75218c59f
Valto Moisio <valto.moisio@pilvit.fi>
2022-04-25 18:12:54 +0300

    Add google authorization

---
+++ frontend/src/components/auth/loginWithGoogle.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+
+const LoginWithGoogle = ({ setLoginData }) => {
+    const handleLogin = async (googleData) => {
+        const res = await fetch('api/login/google', {
+            method: 'POST',
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await res.json()
+        setLoginData(data)
+    }
+
+    const handleFailure = (result) => {
+        console.log(result)
+    }
+
+    return <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Log in with Google"
+        onSuccess={handleLogin}
+        onFailure={handleFailure}
+        cookiePolicy='single_host_origin'
+        />
+}
+
+export default LoginWithGoogle
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import LoginWithGoogle from '../components/auth/loginWithGoogle'
 
 const Login = () => {
+    const [loginData, setLoginData] = useState(
+        localStorage.getItem('loginData')
+            ? JSON.parse(localStorage.getItem('loginData'))
+            : null
+    )
+
+    const handleLogout = () => {
+        localStorage.removeItem('loginData')
+        setLoginData(null)
+    }
+
     return (
         <div className='loginPage'>
-            <p>Login</p>
+            {
+                loginData ? (
+                    <div>
+                        <p>Logged in as {loginData.email}</p>
+                        <button onClick={handleLogout}>Logout</button>
+                    </div>
+                ) : <LoginWithGoogle setLoginData={setLoginData}/>
+            }
         </div>
     )
 }
