fad00d63da5580b07f87197cfb110add0099d573
Valto Moisio <valto.moisio@pilvit.fi>
2022-05-05 16:30:37 +0300

    Revert "Install devdependencies for testing"
    This reverts commit 0d5aa91dbf520270d7c306907bc6693f9f804e31.

--- backend/tests/services/sport.service.test.js
+++
@@ -1,7 +0,0 @@
-import * as sportService from '../../src/services/sport.service.js'
-
-describe('Test sport.service.js', () => {
-    test('Get sports', () => {
-        expect(sportService.get()).toBeInstanceOf(Array)
-    })
-})
